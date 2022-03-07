6cd6d37b85380ff7e49b6ce847d34877b5e01a32
Lukas Bombach <lukas.bombach@googlemail.com>
2022-03-07 15:37:28 +0100

    added .brew

--- src/apps.ts
+++ src/apps.ts
@@ -3,7 +3,6 @@ import chokidar from "chokidar";
 chokidar
   .watch("/Applications/**/*", {
     ignored: [/\/\./, /\.app\/.+/],
-    // followSymlinks: false,
   })
   .on("all", (event, path) => {
     console.log(event, path);
